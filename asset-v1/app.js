d796a3c9f834fd2f79ad9e34d7ba13530c5e598d
mrcookeee <official.inkredaman@gmail.com>
2026-03-02 16:50:19 -0600

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -1,101 +1,101 @@
 /* 0.5.4 2021-04-23 21:06:13 */
 var app = new function() {
     this.name = "Alpha", this.version = "1", this.date = "2009", this.folder = "asset-v1/", this.looptime = 5333, this.bpm = 90, this.totalframe = 260, this.nbpolo = 7, this.nbloopbonus = 2, this.bonusloopA = !1, this.bonusendloopA = !1, this.recmaxloop = 34, this.recminloop = 4, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#121212", this.col0 = "#919191", this.col1 = "#5f5f5f", this.col2 = "#4b4b4b", this.col3 = "#373737", this.col4 = "#232323", this.animearray = [{
-        name: "1_lead",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "2_deux",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "3_kosh",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "4_shpok",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "5_tom",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "6_nouana",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "7_scratch",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "8_trill",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "9_bass",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "10_uh",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "11_nugu",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "12_guit",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "13_tromp",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "14_pouin",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "15_tung",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "16_aoun",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "17_hum",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "18_get",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "19_tellme",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }, {
-        name: "20_make",
+        name: "temp_polo",
         color: "828282",
         uniqsnd: !0
     }], this.bonusarray = [{
-        name: "The Choir",
+        name: "?",
         src: "v1-b1-choir-hb.mp4",
-        code: "2,4,8,11,15",
+        code: "1,2,3,4,5",
         sound: "bonus-choir",
         aspire: "aspire"
     }, {
-        name: "The Sailor",
+        name: "?",
         src: "v1-b2-sailor-hb.mp4",
-        code: "10,11,14,15,20",
+        code: "6,7,8,9,10",
         sound: "bonus-sailor",
         aspire: "aspire-sailor"
     }, {
-        name: "Santa",
+        name: "?",
         src: "v1-b3-santa-hb.mp4",
-        code: "1,9,13,14,16",
+        code: "11,12,13,14,15",
         sound: "bonus-santa",
         aspire: "aspire-santa"
     }];
